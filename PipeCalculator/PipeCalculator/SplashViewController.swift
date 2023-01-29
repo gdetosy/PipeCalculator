2f41d30a5c9b76c6b97ee05acae25dd953b69fbe
gdetosy <tosyjdm@gmail.com>
2023-01-29 21:52:13 +0300

    fix https -> http and and random qoute

--- PipeCalculator/PipeCalculator/SplashViewController.swift
+++ PipeCalculator/PipeCalculator/SplashViewController.swift
@@ -5,17 +5,37 @@
 //  Created by tosy on 28.01.23.
 //
 
+import Alamofire
+import SwiftyJSON
 import UIKit
 
 class SplashViewController: UIViewController {
+    @IBOutlet var label: UILabel!
+
+    let url = "http://numbersapi.com/2/29/date?json"
+   
     override func viewDidLoad() {
+        getQoute(url: url)
         super.viewDidLoad()
-//       MARK: -  Splash with qoute and easy animation transition 2 next view
+
+        //       MARK: -  Splash with qoute and easy animation transition 2 next view
         
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
             self.performSegue(withIdentifier: "Splash", sender: nil)
-            
-            
-            }
+        }
+    }
+
+    // MARK: - random fact on this date
+
+    func getQoute(url: String) {
+        AF.request(url).responseJSON { [weak self] response in switch response.result {
+        case .success(let value):
+            let json = JSON(value)
+            print(json)
+            self?.label.text = "\(json["text"])"
+        case .failure(let error):
+            print(error)
+        }
+        }
     }
 }
