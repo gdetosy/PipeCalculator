ab99139244657089f83282a60be2769343a3a37c
gdetosy <tosyjdm@gmail.com>
2023-02-05 12:42:15 +0300

    some test logic with button

--- PipeCalculator/PipeCalculator/MainViewController.swift
+++ PipeCalculator/PipeCalculator/MainViewController.swift
@@ -7,9 +7,13 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITextViewDelegate {
  
-    
+    private func textFieldDidBeginEditing(_ textField: UITextField) {
+            if textField == diametrTextField {
+                print("You edit myTextField")
+            }
+        }
     
     
     @IBOutlet var diametrTextField: UITextField!
@@ -23,37 +27,41 @@ class MainViewController: UIViewController {
     @IBOutlet var nextButton: UIButton!
 
     @IBAction func Diametr(_ sender: Any) {
-        activeBtn()
+if diametrTextField.text!.isEmpty || tolshinaTextField.text!.isEmpty {
+    // set the button state to disabled
+    nextButton.isEnabled = false
+  // otherwise
+  } else {
+    // set the button state to enabled
+    nextButton.isEnabled = true
+    }
+  
 
-        guard let diametr = Double(diametrTextField.text!), Double(diametrTextField.text!) ?? 0 > 0
+        guard let diametr = Double(diametrTextField.text!)
         else { diametrTextField.text?.removeAll()
             return
-       
         }
- 
+
         print(diametr)
     }
-    
+//
     @IBAction func tolshina(_ sender: UITextField) {
-       
-        guard let tolshina = Double(tolshinaTextField.text!), Double(tolshinaTextField.text!) ?? 0 < Double(diametrTextField.text!) ?? 0,
-              Double(tolshinaTextField.text!) ?? 0 > 0
+
+        guard let tolshina = Double(tolshinaTextField.text!), Double(tolshinaTextField.text!) ?? 0 < Double(diametrTextField.text!) ?? 0
+      
         else { tolshinaTextField.text?.removeAll()
             return
         }
         print(tolshina)
     }
-    
+
     @IBAction func dlina(_ sender: UITextField) {
         guard let dlina = Double(dlinaTextField.text!) else { dlinaTextField.text?.removeAll()
             return
         }
         print(dlina)
     }
-    
-    @IBAction func massa(_ sender: UITextField) {
 
-    }
     @IBAction func raschet(_ sender: UIButton) {
      massa()
         
@@ -61,26 +69,27 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+     
+       
     }
 //    massa = ((diametr - stenka) * stenka * 0,02466 * metraj) / 1000
 
     func massa() {
-        let diametr = Double(diametrTextField.text!)
-        let stenka = Double(tolshinaTextField.text!)
+       guard let diametr = Double(diametrTextField.text!),
+        let stenka = Double(tolshinaTextField.text!),
         let metraj = Double(dlinaTextField.text!)
-        
-        let massa1 = (((diametr ?? 0) - stenka!) * stenka! * 0.02466 * metraj!) / 1000
+        else {return}
+        let massa1 = (((diametr) - stenka) * stenka * 0.02466 * metraj) / 1000
         heightTextField.text = "\(massa1)"
         print(massa1)
     }
-    func activeBtn() {
+//    func activeBtn() {
 //    { if diametrTextField.text!.isEmpty == false, tolshinaTextField.text!.isEmpty == false, dlinaTextField.text!.isEmpty == false {
-        nextButton.isHidden = diametrTextField.text!.isEmpty && tolshinaTextField.text!.isEmpty &&
-        dlinaTextField.text!.isEmpty
-    }
-    
+//        nextButton.isHidden = diametrTextField.text!.isEmpty && tolshinaTextField.text!.isEmpty &&
+//        dlinaTextField.text!.isEmpty
+//    }
+//    textField.addTarget(self, action: #selector(ViewController.textFieldDidChange(_:)),
+//                              for: .editingChanged)
     
     /*
     // MARK: - Navigation
