6927c0d98c5144ddd33d64d6cc61a7a3af0e9795
gdetosy <tosyjdm@gmail.com>
2023-02-06 20:47:53 +0300

    some busines logic upd

--- PipeCalculator/PipeCalculator/MainViewController.swift
+++ PipeCalculator/PipeCalculator/MainViewController.swift
@@ -7,15 +7,7 @@
 
 import UIKit
 
-class MainViewController: UIViewController, UITextViewDelegate {
- 
-    private func textFieldDidBeginEditing(_ textField: UITextField) {
-            if textField == diametrTextField {
-                print("You edit myTextField")
-            }
-        }
-    
-    
+class MainViewController: UIViewController {
     @IBOutlet var diametrTextField: UITextField!
     
     @IBOutlet var tolshinaTextField: UITextField!
@@ -27,16 +19,15 @@ class MainViewController: UIViewController, UITextViewDelegate {
     @IBOutlet var nextButton: UIButton!
 
     @IBAction func Diametr(_ sender: Any) {
-if diametrTextField.text!.isEmpty || tolshinaTextField.text!.isEmpty {
-    // set the button state to disabled
-    nextButton.isEnabled = false
-  // otherwise
-  } else {
-    // set the button state to enabled
-    nextButton.isEnabled = true
-    }
-  
+        if diametrTextField.text!.isEmpty || tolshinaTextField.text!.isEmpty || dlinaTextField.text!.isEmpty {
+            nextButton.isEnabled = false
+ 
+        } else {
+            massa()
 
+            nextButton.isEnabled = true
+        }
+  
         guard let diametr = Double(diametrTextField.text!)
         else { diametrTextField.text?.removeAll()
             return
@@ -44,9 +35,9 @@ if diametrTextField.text!.isEmpty || tolshinaTextField.text!.isEmpty {
 
         print(diametr)
     }
+
 //
     @IBAction func tolshina(_ sender: UITextField) {
-
         guard let tolshina = Double(tolshinaTextField.text!), Double(tolshinaTextField.text!) ?? 0 < Double(diametrTextField.text!) ?? 0
       
         else { tolshinaTextField.text?.removeAll()
@@ -62,42 +53,34 @@ if diametrTextField.text!.isEmpty || tolshinaTextField.text!.isEmpty {
         print(dlina)
     }
 
-    @IBAction func raschet(_ sender: UIButton) {
-     massa()
-        
-    }
+    // MARK: - raschet po kursu
+
+    @IBAction func raschet(_ sender: UIButton) {}
     
     override func viewDidLoad() {
         super.viewDidLoad()
-     
-       
     }
-//    massa = ((diametr - stenka) * stenka * 0,02466 * metraj) / 1000
+
+    // MARK: - formula rascheta massa
 
     func massa() {
-       guard let diametr = Double(diametrTextField.text!),
-        let stenka = Double(tolshinaTextField.text!),
-        let metraj = Double(dlinaTextField.text!)
-        else {return}
-        let massa1 = (((diametr) - stenka) * stenka * 0.02466 * metraj) / 1000
+        guard let diametr = Double(diametrTextField.text!),
+              let stenka = Double(tolshinaTextField.text!),
+              let metraj = Double(dlinaTextField.text!)
+        else { return }
+        let massa1 = ((diametr - stenka) * stenka * 0.02466 * metraj) / 1000
+//      return massa1
         heightTextField.text = "\(massa1)"
         print(massa1)
     }
-//    func activeBtn() {
-//    { if diametrTextField.text!.isEmpty == false, tolshinaTextField.text!.isEmpty == false, dlinaTextField.text!.isEmpty == false {
-//        nextButton.isHidden = diametrTextField.text!.isEmpty && tolshinaTextField.text!.isEmpty &&
-//        dlinaTextField.text!.isEmpty
-//    }
-//    textField.addTarget(self, action: #selector(ViewController.textFieldDidChange(_:)),
-//                              for: .editingChanged)
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    /*
+     // MARK: - Navigation
+
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+     }
+     */
 }
