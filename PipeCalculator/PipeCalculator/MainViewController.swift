7deb7108ab837c62eb2e6b9b70a1329b9dbe547d
gdetosy <tosyjdm@gmail.com>
2023-02-06 22:20:58 +0300

    rounded busines logic

--- PipeCalculator/PipeCalculator/MainViewController.swift
+++ PipeCalculator/PipeCalculator/MainViewController.swift
@@ -9,27 +9,27 @@ import UIKit
 
 class MainViewController: UIViewController {
     @IBOutlet var diametrTextField: UITextField!
-    
+
     @IBOutlet var tolshinaTextField: UITextField!
-    
+
     @IBOutlet var dlinaTextField: UITextField!
-    
+
     @IBOutlet var heightTextField: UITextField!
-    
+
     @IBOutlet var nextButton: UIButton!
 
     @IBAction func Diametr(_ sender: Any) {
         if diametrTextField.text!.isEmpty || tolshinaTextField.text!.isEmpty || dlinaTextField.text!.isEmpty {
             nextButton.isEnabled = false
- 
+
         } else {
             massa()
 
             nextButton.isEnabled = true
         }
-  
-        guard let diametr = Double(diametrTextField.text!),
-              Double(diametrTextField.text!) ?? 0 >= 21.3
+
+        guard let diametr = Float(diametrTextField.text!),
+              Float(diametrTextField.text!) ?? 0 >= 21.3
         else { diametrTextField.text?.removeAll()
             return
         }
@@ -39,8 +39,8 @@ class MainViewController: UIViewController {
 
 //
     @IBAction func tolshina(_ sender: UITextField) {
-        guard let tolshina = Double(tolshinaTextField.text!), Double(tolshinaTextField.text!) ?? 0 < Double(diametrTextField.text!) ?? 0
-      
+        guard let tolshina = Float(tolshinaTextField.text!), Float(tolshinaTextField.text!) ?? 0 < Float(diametrTextField.text!) ?? 0
+
         else { tolshinaTextField.text?.removeAll()
             return
         }
@@ -48,7 +48,7 @@ class MainViewController: UIViewController {
     }
 
     @IBAction func dlina(_ sender: UITextField) {
-        guard let dlina = Double(dlinaTextField.text!) else { dlinaTextField.text?.removeAll()
+        guard let dlina = Float(dlinaTextField.text!) else { dlinaTextField.text?.removeAll()
             return
         }
         print(dlina)
@@ -57,7 +57,7 @@ class MainViewController: UIViewController {
     // MARK: - raschet po kursu
 
     @IBAction func raschet(_ sender: UIButton) {}
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -65,13 +65,12 @@ class MainViewController: UIViewController {
     // MARK: - formula rascheta massa
 
     func massa() {
-        guard let diametr = Double(diametrTextField.text!),
-              let stenka = Double(tolshinaTextField.text!),
-              let metraj = Double(dlinaTextField.text!)
+        guard let diametr = Float(diametrTextField.text!),
+              let stenka = Float(tolshinaTextField.text!),
+              let metraj = Float(dlinaTextField.text!)
         else { return }
         let massa1 = ((diametr - stenka) * stenka * 0.02466 * metraj) / 1000
-//      return massa1
-        heightTextField.text = "\(massa1)"
+        heightTextField.text = "\(round(massa1 * 100000) / 100000)"
         print(massa1)
     }
 
