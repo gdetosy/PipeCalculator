6b44267cb7c9fb8dc13e82d50a5c8478c207c075
gdetosy <tosyjdm@gmail.com>
2023-02-22 16:44:58 +0300

    json usd eur

--- PipeCalculator/PipeCalculator/CurrencyViewController.swift
+++ PipeCalculator/PipeCalculator/CurrencyViewController.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class CurrencyViewController: UIViewController {
     var diametr: Float = 0
     var tolshina: Float = 0
     var dlina: Float = 0
     var height: Float = 0
+    let url = "https://www.nbrb.by/api/exrates/rates?periodicity=0"
+   
     @IBOutlet var pipeLabel: UILabel!
     
     @IBOutlet var lenghtHeightLabel: UILabel!
@@ -22,22 +26,32 @@ class CurrencyViewController: UIViewController {
     
     @IBOutlet var totalPriceLabel: UILabel!
     
+    @IBOutlet var usdLabel: UILabel!
+    
+    @IBOutlet var eurLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-print(height)
+        getPrice(url: url)
         // Do any additional setup after loading the view.
     }
     
     @IBAction func priceTextField(_ sender: UITextField) {}
     
     @IBAction func segmentControl(_ sender: Any) {}
-    /*
-     // MARK: - Navigation
+  
+    func getPrice(url: String) {
+        AF.request(url).responseJSON { [weak self] response in switch response.result {
+        case .success(let value):
+            let json = JSON(value)
+           
+            self?.usdLabel.text = "USD: \(json[7, "Cur_OfficialRate"]) "
+            self?.eurLabel.text = "EUR: \(json[9, "Cur_OfficialRate"]) "
+            
+        case .failure:
+            print("error")
+        }
+        }
+    }
 
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destination.
-         // Pass the selected object to the new view controller.
-     }
-     */
 }
