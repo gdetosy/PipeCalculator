2f7c6955f26c269ebd7d2f1856dc669418abc519
gdetosy <tosyjdm@gmail.com>
2023-03-01 21:21:05 +0300

    98% business ok

--- PipeCalculator/PipeCalculator/CurrencyViewController.swift
+++ PipeCalculator/PipeCalculator/CurrencyViewController.swift
@@ -17,11 +17,11 @@ class CurrencyViewController: UIViewController {
     var usd: String = ""
     var eur: String = ""
     var finishPrice: Float = 0
-    
+    var segmentos = 0
     @IBOutlet var pipeLabel: UILabel!
     
     @IBOutlet var lenghtHeightLabel: UILabel!
-   
+    
     @IBOutlet var priceTextField: UITextField!
     
     @IBOutlet var segmentConrol: UISegmentedControl!
@@ -34,47 +34,56 @@ class CurrencyViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        totalPriceLabel.text = "Введите цену в BYN"
         getParametrs()
     }
     
     @IBAction func priceTextField(_ sender: UITextField) {
         if priceTextField.text?.isEmpty == true {
-//            finishPrice = 0
+            //            finishPrice = 0
             totalPriceLabel.text = "Введите цену в BYN"
         } else {
             guard let price = Float(priceTextField.text!) else { return }
             let totalPrice = (round((height * price) * 1000) / 1000)
             finishPrice = totalPrice
-            totalPriceLabel.text = "Цена \(finishPrice) рублей без НДС"
+            segmention()
         }
     }
-
+    
     @IBAction func segmentControl(_ sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex {
         case 0:
-           
-            totalPriceLabel.text = "Цена \(finishPrice) рублей без НДС"
-            
+            segmentos = 0
+            segmention()
         case 1:
-           
-            totalPriceLabel.text = "Цена \(finishPrice * Float(usd)!) USD без НДС"
-            
+            segmentos = 1
+            segmention()
         case 2:
-            
-            totalPriceLabel.text = "Цена \(finishPrice * Float(eur)!) EUR без НДС"
-            
+            segmentos = 2
+            segmention()
         default:
             print("lol")
         }
     }
-  
+    
     private func getParametrs() {
         pipeLabel.text = " Труба \(diametr) x \(tolshina) мм Сталь 3пс"
         
         lenghtHeightLabel.text = "Длинна \(dlina)м, вес  \(height)тн"
-       
+        
         usdLabel.text = "1 USD = \(usd) руб"
         eurLabel.text = "1 EUR = \(eur) руб"
+    }
+
+    func segmention() {
+        if segmentos == 0 {
+            totalPriceLabel.text = "Цена \(finishPrice) рублей без НДС"
+        }
+        if segmentos == 1 {
+            totalPriceLabel.text = "Цена \(finishPrice * Float(usd)!) USD без НДС"
+        }
+        if segmentos == 2 {
+            totalPriceLabel.text = "Цена \(finishPrice * Float(eur)!) EUR без НДС"
+        }
     }
 }
